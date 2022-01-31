efe8ba29fcb8bbe99cc9756ffbd2b3aaeebb8276
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-01-31 13:02:21 -0500

    Exercises 03 in progress

--- src/edu/realemj/Exercises_02/Wasteland.java
+++ src/edu/realemj/Exercises_02/Wasteland.java
@@ -4,6 +4,19 @@ public class Wasteland {
     public static void main(String [] args) {
         NPC bob = new NPC();
         NPC sue = new NPC(93, 61, 'F');
+        // CAN'T DO THIS: NPC joe = new NPC(56, 34);
+
+        bob.setHealth(70);
+        int bh = bob.getHealth();
+        System.out.println(bh);
+
+        // Person <-- Human <-- NPC
+        //Person p = new NPC();
+        //p = new Human();
+        // LOTS OF CODE LATER...
+        //if(p instanceof NPC) {
+        //    NPC n = (NPC)p;
+        //}
         
 
     }    
---
+++ src/edu/realemj/Exercises_03/FoodRationer.java
@@ -0,0 +1,28 @@
+package edu.realemj.Exercises_03;
+
+import edu.realemj.Exercises_02.NPC;
+
+public class FoodRationer {
+    // These were just to demonstrate what
+    // the default values of variables are.
+    //private int x;
+    //private double y;
+    //private char c;
+    //private NPC n;
+    public static void main(String [] args) {
+        // Ditto for above; not really part of
+        // main exercise
+        //FoodRationer r = new FoodRationer();
+        //System.out.println("HELLO" + r);
+
+        // Get data for calculation
+        int numPeople = 5;
+        double rations = 2.0;
+        double totalFood = 100.0;
+
+
+
+
+    }
+    
+}
