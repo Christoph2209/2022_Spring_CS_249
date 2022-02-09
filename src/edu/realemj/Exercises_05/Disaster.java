b06d991571cab65dfe75edaf6d6fb00352ac0b98
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-02-09 13:00:43 -0500

    Selection exercises

--- src/edu/realemj/Exercises_05/Disaster.java
+++ src/edu/realemj/Exercises_05/Disaster.java
@@ -1,6 +1,7 @@
 package edu.realemj.Exercises_05;
 
 public enum Disaster {
+    NO_DISASTER,
     DYSENTERY,
     CHOLERA,
     WAGON_WHEEL_BROKEN,
--- src/edu/realemj/Exercises_05/DisasterCheck.java
+++ src/edu/realemj/Exercises_05/DisasterCheck.java
@@ -2,8 +2,62 @@ package edu.realemj.Exercises_05;
 
 public class DisasterCheck {
     public static void main(String [] args) {
-        Disaster disaster = Disaster.LOST_FOOD;
+        Disaster disaster = Disaster.WAGON_AXLE_BROKEN;
+
+        if(disaster == Disaster.LOST_FOOD) {
+            System.out.println("We're going to STARVE!");
+        }
+
         
+        // ORIGINAL SYNTAX
+        switch(disaster) {
+            case LOST_FOOD:                
+                System.out.println("No food!");
+                break;
+            case CHOLERA:
+            case DYSENTERY: 
+                System.out.println("You're sick!");
+                break;
+            case WAGON_AXLE_BROKEN:
+            case WAGON_WHEEL_BROKEN: 
+                System.out.println("Can't move!");
+                break;
+            default:
+                System.out.println("All good!");
+                break;
+        }
+
+        // NEW SYNTAX
+        switch(disaster) {
+            case LOST_FOOD -> System.out.println("No food!");              
+            case CHOLERA, DYSENTERY -> System.out.println("You're sick!");            
+            case WAGON_AXLE_BROKEN, WAGON_WHEEL_BROKEN -> {
+                System.out.println("Can't move!");
+                System.out.println("Wagon busted!");
+            }
+            default -> System.out.println("All good!");                
+        }   
+        
+        // SWITCH EXPRESSION
+        int totalFood = 100;
+        boolean canMove = true;
+
+        String statusReport = switch(disaster) {
+            case LOST_FOOD -> {
+                totalFood -= 10;
+                yield "Lost food! Total food is now " + totalFood;
+            }
+            case CHOLERA, DYSENTERY -> "You're sick!";
+            case WAGON_AXLE_BROKEN, WAGON_WHEEL_BROKEN -> {
+                canMove = false;
+                yield "Wagon broken!";
+            }
+            default -> "All good!";
+        };
+
+        System.out.println("STATUS: " + statusReport);
+        //System.out.println("Can move? " + canMove);
+        System.out.println(canMove ? "Can move!" : "We're stuck!");
     }
     
 }
