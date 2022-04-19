108d83e2ed45d0b3b0cbc8ed01f0e6e10802a57a
Christoph2209 <93446223+Christoph2209@users.noreply.github.com>
2022-04-19 13:25:09 -0400

    starting assign07

---
+++ src/edu/soravic/Assign07/TreasureMap.java
@@ -0,0 +1,20 @@
+package edu.soravic.Assign07;
+
+public class TreasureMap extends CharImage{
+    protected int row;
+    protected int col;
+
+    public TreasureMap(int rowCnt, int colCnt, char fillChar)
+    {
+        super(rowCnt,colCnt,fillChar);
+    }
+
+    public void clear()
+    {
+        row = 0;
+        col = 0;
+        super.clear();
+    }
+
+    public int 
+}
